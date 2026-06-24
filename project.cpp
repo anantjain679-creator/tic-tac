5eda84619c817ee668a6cf0a3f071ae5569ee91f
hollowpurple <anantjain679@gmail.com>
2026-06-24 23:18:01 +0530

    board is created

---
+++ project.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+char board [3][3] = {{'X', 'O', 'X'}, {'X', 'O', 'X'}, {'O', 'O', 'X'} } ;
+
+for(int i = 0; i<3 ; i++) {
+    cout << board[i][0] << board[i][1] << board [i][2] << endl ;
+}
+
+
+
+
+
+return 0 ;
+} 
+
+
