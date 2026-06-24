24d369c61bd6be7b586b06056afc331c381d0d08
hollowpurple <anantjain679@gmail.com>
2026-06-24 23:49:53 +0530

    final changes made on board

--- project.cpp
+++ project.cpp
@@ -9,11 +9,18 @@ for(int i = 0; i<3 ; i++) {
     cout << board[i][0] << board[i][1] << board [i][2] << endl ;
 }
 
+cout << "   |   |   "  << endl ;
+cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl ;
+cout << " __|___|__"  << endl ;
+cout << "   |   |   "  << endl ;
+cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl ;
+cout << " __|___|__"  << endl ;
+cout << "   |   |   "  << endl ;
+cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl ;
+cout << " __|___|__"  << endl ;
+cout << "   |   |   "  << endl ;
 
 
-
-
-return 0 ;
 } 
 
 
